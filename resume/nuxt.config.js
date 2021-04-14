b18cb68a7de1d9fbe490d387ce049e49a35a7107
Giorgio Lasala <giorgio.lasala@eng.it>
2021-04-14 22:17:38 +0200

    prettier and pipeline

--- resume/nuxt.config.js
+++ resume/nuxt.config.js
@@ -6,67 +6,64 @@ export default {
   head: {
     title: 'resume',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
-        rel: "stylesheet",
-        type: "text/css",
+        rel: 'stylesheet',
+        type: 'text/css',
         href:
-          "https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900"
+          'https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:100,200,300,400,500,600,700,800,900',
       },
       {
-        rel: "stylesheet",
-        type: "text/css",
+        rel: 'stylesheet',
+        type: 'text/css',
         href:
-          "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i"
+          'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i',
       },
       {
-        rel: "stylesheet",
-        type: "text/css",
+        rel: 'stylesheet',
+        type: 'text/css',
         href:
-          "https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css"
+          'https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css',
       },
       {
-        rel: "stylesheet",
-        type: "text/css",
+        rel: 'stylesheet',
+        type: 'text/css',
         href:
-          "https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css"
+          'https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css',
       },
       {
-        rel: "stylesheet",
-        type: "text/css",
+        rel: 'stylesheet',
+        type: 'text/css',
         href:
-          "https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
+          'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
         integrity:
-          "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB",
-        crossorigin: "anonymous"
+          'sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB',
+        crossorigin: 'anonymous',
       },
       {
-        rel: "stylesheet",
-        type: "text/css",
-        href: "https://use.fontawesome.com/releases/v5.1.0/css/all.css",
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: 'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
         integrity:
-          "sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt",
-        crossorigin: "anonymous"
-      }
-    ]
+          'sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt',
+        crossorigin: 'anonymous',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    '@/assets/resume.css'
-  ],
+  css: ['@/assets/resume.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -75,7 +72,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    '@nuxtjs/color-mode'
+    '@nuxtjs/color-mode',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -84,28 +81,27 @@ export default {
       'nuxt-i18n',
       {
         /* module options */
-      }
+      },
     ],
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
   ],
 
   i18n: {
-    locales: ["en", "it"],
-    defaultLocale: "it",
+    locales: ['en', 'it'],
+    defaultLocale: 'it',
     vueI18n: {
-      fallbackLocale: "en",
+      fallbackLocale: 'en',
       messages: {
-        en: require("./locales/en.json"),
-        es: require("./locales/it.json")
-      }
-    }
+        en: require('./locales/en.json'),
+        es: require('./locales/it.json'),
+      },
+    },
   },
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
