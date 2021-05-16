a51ef19294ff983bf357fe26feb2a3762ed1b9b2
salem84 <giorgio.lasala@gmail.com>
2021-05-17 00:09:09 +0200

    update skills

--- resume/components/Sections/Skills.vue
+++ resume/components/Sections/Skills.vue
@@ -5,13 +5,13 @@
     <div class="subheading mb-3">{{ $t('skills.iconsTitle') }}</div>
     <ul class="list-inline list-icons" style="text-align: center">
       <li v-for="icon in $t('skills.devIcons')" :key="icon" class="list-inline-item">
-        <i :class="icon"></i>
+        <i :class="icon" class="colored"></i>
       </li>
     </ul>
 
-    <div class="subheading mb-3">{{ $t('skills.workflow') }}</div>
+    <div class="subheading mb-3">{{ $t('skills.certificationsTitle') }}</div>
     <ul class="fa-ul mb-0">
-      <li v-for="item in $t('skills.itemList')" :key="item">
+      <li v-for="item in $t('skills.certifications')" :key="item">
         <i class="fa-li fa fa-check"></i>
         {{ item }}
       </li>
